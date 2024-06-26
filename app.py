acc28b30a72eec74d3ed05e6537f19a9dbe5f3f6
Your Name <sneha.sathish_ext@ltts.com>
2024-06-26 17:01:32 +0530

    adding input probes

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ import streamlit as st
 import json
 import main as m
 
+st.set_page_config(layout='wide')
+
 st.title("garak")
 
 with open("model_names.json", "r") as read_content: 
     model_names=json.load(read_content)['model_names']
+
+with open("probes.json", "r") as read_content: 
+    probes=json.load(read_content)['probes']
 
 #initialization
 if "model_type" not in st.session_state:
@@ -17,8 +22,8 @@ if "model_name" not in st.session_state:
 if "generations" not in st.session_state:
     st.session_state["generations"] = None
 
-if "submit" not in st.session_state:
-     st.session_state["submit"] = None
+# if "submit" not in st.session_state:
+#      st.session_state["submit"] = None
 
 # if "probes" not in st.session_state:
 #      st.session_state["probes"] = None
@@ -30,16 +35,13 @@ if not st.session_state["model_type"]:
 if not st.session_state["model_name"]:
     model_name = st.text_input(label = " Enter Model Type ",key="model_name")
 
-# if not st.session_state["probes "]:
-#     st.session_state["probes "] =st.text_input("Enter Webpage URL", type="default")
+if not st.session_state["model_type"]:
+        model_type = st.selectbox(label= " Select the probe ",options = probes )
 
 if not st.session_state["generations"]:
     generations = st.text_input(label = " Enter Number of Generations ")
 
-submit = False
-submit = st.button("Click to run test")
-
-if st.session_state[submit]:
-     print("works")
+if st.button("Start test"):
+    print(model_type,generations,model_name,probes)
 
 # m.run_garak_command(model_type,model_name,generations)
