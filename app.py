bd6df58247b323650c40b525f0ac8a1450dd7bf7
Sneha Sathish Kumar <snehasathish44@gmail.com>
2024-06-26 07:07:24 +0530

    adding changes

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ st.title("garak")
 with open("model_names.json", "r") as read_content: 
     model_names=json.load(read_content)['model_names']
 
+#initialization
 if "model_type" not in st.session_state:
     st.session_state["model_type"] = None
 
@@ -16,12 +17,29 @@ if "model_name" not in st.session_state:
 if "generations" not in st.session_state:
     st.session_state["generations"] = None
 
-if not st.session_state["uploaded_file_sum"]:
-        st.session_state["uploaded_file_sum"] = st.file_uploader("Upload your file here")
+if "submit" not in st.session_state:
+     st.session_state["submit"] = None
 
-model_type = st.selectbox(label= " Select your model Name" , options = model_names )
-model_name = st.text_input(label = " Enter Model Type ")
-generations = st.text_input(label = " Enter Number of Generations ")
+# if "probes" not in st.session_state:
+#      st.session_state["probes"] = None
 
-if model_type!
-m.run_garak_command(model_type,model_name,generations)
+#input 
+if not st.session_state["model_type"]:
+        model_type = st.selectbox(label= " Select your model Name",options=model_names )
+
+if not st.session_state["model_name"]:
+    model_name = st.text_input(label = " Enter Model Type ",key="model_name")
+
+# if not st.session_state["probes "]:
+#     st.session_state["probes "] =st.text_input("Enter Webpage URL", type="default")
+
+if not st.session_state["generations"]:
+    generations = st.text_input(label = " Enter Number of Generations ")
+
+submit = False
+submit = st.button("Click to run test")
+
+if st.session_state[submit]:
+     print("works")
+
+# m.run_garak_command(model_type,model_name,generations)
